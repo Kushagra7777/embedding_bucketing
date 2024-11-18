cc8a5574166808f93c2f6a818ba01deb0268a27b
Kushagra  Srivastav <76401345+Kushagra7777@users.noreply.github.com>
2024-11-18 19:50:23 +0530

    Update arch_Wscripted_Curator.py

--- arch_Wscripted_Curator.py
+++ arch_Wscripted_Curator.py
@@ -1,10 +1,16 @@
-## // Wscripted- Personal Curation Agent
+# -*- coding: utf-8 -*-
+"""
+// aolabs.ai software >ao_core/Arch.py (C) 2023 Animo Omnis Corporation. All Rights Reserved.
+"""
+
+
+## // WScripted- Personal Curation Agent
 # 
 # Takes in Genre, Theme, and Comparative Title (10 binary neurons each) for a piece of content to offer the user a recommendation, 
 # learning from the users subsequent interaction.
 
-description = "Wscripted- Personal Curation Agent - demo #1"
-arch_i = [7, 21, 21]               # 10 binary neurons each for Genre, Theme, and Comparative Title
+description = "WScripted- Personal Curation Agent - demo #1"
+arch_i = [7, 21, 21]               
 arch_z = [10]                       # 10 binary neurons for output-- if the sum of the response >7, positive recommendation
 arch_c = []
 connector_function = "full_conn"
